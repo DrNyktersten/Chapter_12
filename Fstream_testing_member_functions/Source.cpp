903764656d406cc06ab7f605b3231366d76d1bb1
DrNyktersten <kurmanbekb001@gmail.com>
2022-07-19 23:56:41 -0500

    using member functions

---
+++ Fstream_testing_member_functions/Source.cpp
@@ -0,0 +1,13 @@
+//This program demonstrates the return value of the stream
+//object error testing member functions. 
+#include <iostream>
+#include <fstream>
+using namespace std; 
+
+//Function prototype 
+void showState(fstream&)
+
+int main()
+{
+	int num = 10; 
+}
